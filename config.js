1a78d69dc2cfe476c2e09a7c5da05391dd344449
Toadoshi <toadoshi09@gmail.com>
2026-05-30 10:31:42 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,5 +5,5 @@
  * Cloudflare Worker används bara för download-sidan (/page/).
  */
 const FUM_CONFIG = {
-  WORKER_URL: 'http://82.70.60.250:3000',
+  WORKER_URL: 'https://proxy.freeuploadmanager.org',
 };
